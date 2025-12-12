390053e4291023d9637bb18ac6a80309c011a968
Dilshanthanu <dilshanthanushka98@gmail.com>
2025-12-12 23:40:58 +0530

    Initial Commit

---
+++ WeatherDashboard/WeatherDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDashboardApp.swift
+//  WeatherDashboard
+//
+//  Created by Dilshan Thanushka on 2025-12-12.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
